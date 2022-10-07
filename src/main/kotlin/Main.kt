7ac0ee08e50411326117bf75b2a8e582b7b8d496
Darren <20088411@mail.wit.ie>
2022-10-07 14:30:08 +0100

    Employee-App - Topic 01 - Printing the Details

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,3 +7,25 @@ var paye = 38.5
 var prsi = 5.2
 var annualBonus = 1450.50
 var cycle = 54.33
+
+fun main(args: Array<String>) {
+    println("-------------------------------------------------------------------------")
+    println("|                            Monthly Payslip                            |")
+    println("|-----------------------------------------------------------------------|")
+    println("|                                                                       |")
+    println("|  Employee Name: " + firstName + " " + surname + " (" + gender + ")                       Employee ID: " + employeeId + "  |")
+    println("|                                                                       |")
+    println("|-----------------------------------------------------------------------|")
+    println("|                                                                       |")
+    println("|  PAYMENT DETAILS                       DEDUCTION DETAILS              |")
+    println("|                                                                       |")
+    println("|-----------------------------------------------------------------------|")
+    println("|  Salary: " + grossSalary + "                      PAYE: " + paye + "                     |")
+    println("|  Bonus: " + annualBonus + "                         PRSI: " +  prsi + "                      |")
+    println("|                                        Cycle to Work: " + cycle + "           |")
+    println("|-----------------------------------------------------------------------|")
+    println("|  Gross: " + grossSalary + "                       Total Deductions: " + (paye + prsi + cycle) +"        |")
+    println("|-----------------------------------------------------------------------|")
+    println("|               NET PAY: " + (paye + prsi + cycle) + "                                          |")
+    println("|-----------------------------------------------------------------------|")
+}
